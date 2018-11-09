644a989ded169edb73c70ae9981e702d6484cf39
nguyenhongson123456 <nguyenhongson31011997@gmail.com>
2018-11-09 21:00:22 +0700

    init_project2

--- database/insert.sql
+++ database/insert.sql
@@ -4,12 +4,15 @@ insert into roles(code,name) values('ADMIN','Quản trị hệ thống');
 insert into roles(code,name) values('USER','người dùng');
 insert into roles(code,name) values('MANAGER','Quản Trị');
 
+
 insert into users(username,password,fullname,email,phoneNumber,status)
 values('admin','$2a$10$/RUbuT9KIqk6f8enaTQiLOXzhnUkiwEJRdtzdrMXXwU7dgnLKTCYG','admin','nguyenhong31011997@gmail.com','012456',1);
 insert into users(username,password,fullname,email,phoneNumber,status)
 values('manager','$2a$10$/RUbuT9KIqk6f8enaTQiLOXzhnUkiwEJRdtzdrMXXwU7dgnLKTCYG','Manager','ten31011997@gmail.com','012456',1);
 insert into users(username,password,fullname,email,phoneNumber,status)
 values('nguyenvana','$2a$10$/RUbuT9KIqk6f8enaTQiLOXzhnUkiwEJRdtzdrMXXwU7dgnLKTCYG','nguyen van a','son31011997@gmail.com','012456',1);
+
+
 
 INSERT INTO users_roles(user_id,role_id) VALUES (1,1);
 INSERT INTO users_roles(user_id,role_id) VALUES (2,3);
